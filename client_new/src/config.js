a98a13638762bb86d72c59c3db269f140ba4a093
Brian King <Thebeakeh@gmail.com>
2026-02-06 16:56:32 -0600

    edits to frontend api url to be able to connect to the backend on render

--- client_new/src/config.js
+++ client_new/src/config.js
@@ -1,5 +1,17 @@
+
+
+// localhost
+
+// const config = {
+//     API_URL: 'http://localhost:5000'
+// };
+
+// export default config;
 const config = {
-    API_URL: 'http://localhost:5000'
+  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
 };
 
 export default config;
+
+
+
