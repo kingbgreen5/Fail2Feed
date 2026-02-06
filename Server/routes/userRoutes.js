6f9d2e4e9cf412321377140ce2c55441953098de
Brian King <Thebeakeh@gmail.com>
2026-02-06 14:29:14 -0600

    changed bcrypt to bcrypt.js

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
 require('dotenv').config();
