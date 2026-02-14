5ee4770ba6da4574f2d389c476d431a0b7edd979
Brian King <Thebeakeh@gmail.com>
2026-02-14 15:51:03 -0600

    favorites messing

--- client_new/src/pages/UserCollection.js
+++ client_new/src/pages/UserCollection.js
@@ -4,43 +4,80 @@ import UserFirearmSelect from "../components/UserFirearmSelect";
 import UserFirearmList from "../components/UserFirearmList";
 import AuthForm from "../components/AuthForm";
 
-const UserCollection = () => {
-    // const { user } = useContext(AuthContext);  
-     const { isAuthenticated, user, logout } = useContext(AuthContext);
-    return (
-        <div>
-            <h6>
-                User Role: {user.role} | User ID: {user.id}
-            </h6>
+// const UserCollection = () => {
+//     // const { user } = useContext(AuthContext);  
+//      const { isAuthenticated, user, logout } = useContext(AuthContext);
+//     return (
+//         <div>
+//             <h6>
+//                 User Role: {user.role} | User ID: {user.id}
+//             </h6>
      
 
 
-    {isAuthenticated ? (
-        // ------------------------------LOGGED IN
-          <>         
-           <div>
-            <UserFirearmList />
-            <UserFirearmSelect />
-        </div>
-   </>
-   ):(
+//     {isAuthenticated ? (
+//         // ------------------------------LOGGED IN
+//           <>         
+//            <div>
+//             <UserFirearmList />
+//             <UserFirearmSelect />
+//         </div>
+//    </>
+//    ):(
 
     
-                //  ------------------------------NOT LOGGED IN
-    <>   
+//                 //  ------------------------------NOT LOGGED IN
+//     <>   
+//     <div>
+//     <AuthForm /> 
+//     </div>
+//     </>
+//     )}
+
+
+
+
+  
+  
+//         </div>
+//     );
+// };
+
+
+
+
+
+const UserCollection = () => {
+  const { isAuthenticated, user } = useContext(AuthContext);
+
+  return (
     <div>
-    <AuthForm /> 
+
+      {isAuthenticated ? (
+        <>
+          <h6>
+            User Role: {user.role} | User ID: {user.id}
+          </h6>
+
+          <UserFirearmList />
+          <UserFirearmSelect />
+        </>
+      ) : (
+        <AuthForm />
+      )}
+
     </div>
-    </>
-    )}
-
-
-
-
-  
-  
-        </div>
-    );
+  );
 };
+
+
+
+
+
+
+
+
+
+
 
 export default UserCollection;
