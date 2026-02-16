df41158df2d21e4e30cc4be2e26b22210fb2ef06
Brian King <Thebeakeh@gmail.com>
2026-02-15 21:40:55 -0600

    adding import firearm make and model picker

--- client_new/src/components/ReportForm.js
+++ client_new/src/components/ReportForm.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import config from "../config";
 import AuthContext from "../context/AuthContext";
+import FirearmMakeModelPicker from "./FirearmMakeModelPicker";
 
 
 
